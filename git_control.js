cf6821ba7e54cc13bdbb4237609719623c6d896c
Duong Viet Cuong <vietcuong.it@gmail.com>
2016-07-01 01:25:53 +0700

    Update git_control.js

--- git_control.js
+++ git_control.js
@@ -1,6 +1,6 @@
 /*  Auto deploy with GIT - CI
     Nodejs
-    */
+*/
     
 var http = require('http');
 // Config port
@@ -20,22 +20,19 @@ function handleRequest(request, response){
 // Controller
 dispatcher.onGet("/deploy", function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-
   // Process git pull
-	require('simple-git')().pull(function(err, update) {
-	    
-	    //  Something if you need read summary and make anything, on this example, i just return status like json
-      //  if(update && update.summary.changes) {
-      //    require('child_process').exec('npm restart'); 
-		  //  }
-		  
+  require('simple-git')().pull(function(err, update) {
+	//  Something if you need read summary and make anything, on this example, i just return status like json
+     	//  if(update && update.summary.changes) {
+     	//	require('child_process').exec('npm restart'); 
+	//  }
       if(!err){
         res.end(JSON.stringify(update));
       }
       else{
-			  res.end(JSON.stringify(err));
-		  }
-    });
+      	res.end(JSON.stringify(err));
+      }
+  });
 }); 
 
 
